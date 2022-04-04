4bdd20ec6effde4fadea40674a5d9213245a4a90
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2022-04-04 13:57:08 -0500

    updated polybar and xrandr

--- linux/.config/polybar/launch.sh
+++ linux/.config/polybar/launch.sh
@@ -1,18 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
+# If all your bars have ipc enabled, you can use
+#polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
 killall -q polybar
-
-# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch top and bottom
-MONITOR=HDMI-0 polybar top &
-MONITOR=HDMI-0 polybar bottom &
-
-MONITOR=VGA1 polybar top &
-MONITOR=VGA1 polybar bottom &
-
-
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+export PRIMARY_MONITOR=PRIMARY
+export SECONDARY_MONITOR=SECONDARY
+polybar primary-top 2>&1 | tee -a /tmp/polybar-primary-top.log & disown
+polybar primary-bottom 2>&1 | tee -a /tmp/polybar-primary-bottom.log & disown
+polybar secondary-top 2>&1 | tee -a /tmp/polybar-primary-top.log & disown
+polybar secondary-bottom 2>&1 | tee -a /tmp/polybar-primary-bottom.log & disown
 
 echo "Bars launched..."
