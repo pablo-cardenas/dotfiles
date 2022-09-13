2cf70d9a19baddae8b4c2d06258c4ba6a1ac745e
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2022-09-13 18:15:53 -0500

    Added config alias

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -1,4 +1,3 @@
-# system
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias grep='grep --color=auto'
@@ -6,3 +5,4 @@ alias ls='ls --color=auto -A'
 alias tree='tree -C'
 alias diff='diff --color=auto'
 alias vim='vim -u ~/.vim/vimrc'
+alias config='git --git-dir $HOME/.dotfiles --work-tree $HOME'
