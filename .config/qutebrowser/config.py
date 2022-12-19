cede23a67c90fa3eba0e33cdab35e297548ccf54
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2022-12-19 00:34:06 -0500

    Added pass to qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,3 +3,8 @@ c.tabs.last_close = 'close'
 c.url.default_page = 'about:blank'
 c.url.start_pages = ['about:blank']
 c.zoom.default = '90%'
+
+config.bind('<z><p><l>', 'spawn --userscript qute-pass -p')
+config.bind('<z><o><l>', 'spawn --userscript qute-pass -o')
+config.bind('<z><u><l>', 'spawn --userscript qute-pass -u')
+config.bind('<z><l>', 'spawn --userscript qute-pass -a')
