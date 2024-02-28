2d1259068b3a33063c07cf437ca581f494906fe1
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2024-02-28 02:21:32 -0500

    Update qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,12 +2,19 @@ c.tabs.last_close = 'close'
 c.tabs.tree_tabs = True
 c.url.default_page = 'about:blank'
 c.url.start_pages = ['about:blank']
-c.zoom.default = '90%'
+
+c.tabs.position = 'left'
+c.colors.tabs.even.bg = 'grey'
+c.colors.tabs.odd.bg = 'grey'
+c.tabs.show = 'switching'
+c.statusbar.show = 'in-mode'
+
 config.load_autoconfig()
 
+config.bind(',t', 'config-cycle tabs.show always switching')
 config.bind(',lp', 'spawn --userscript qute-pass -p')
 config.bind(',lo', 'spawn --userscript qute-pass -o')
 config.bind(',lu', 'spawn --userscript qute-pass -u')
 config.bind(',ll', 'spawn --userscript qute-pass -a')
-config.bind(',m', 'spawn mpv {url}')
-config.bind(',M', 'hint links spawn mpv {hint-url}')
+config.bind(',m', 'spawn mpv --audio-device=pulse --ytdl-raw-options=sub-langs="[en.*,es.*,fr.*,pt.*]",write-sub=,write-auto-sub= {url}')
+config.bind(',M', 'hint links spawn mpv --audio-device=alsa/pulse --ytdl-raw-options=sub-langs="[en.*,es.*,fr.*,pt.*]",write-sub=,write-auto-sub= {hint-url}')
