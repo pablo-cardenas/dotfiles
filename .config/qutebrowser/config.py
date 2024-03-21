fb6937efe8a4c89092d9125a3fa7b741a4def1c8
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2024-03-20 22:02:22 -0500

    qutebrowser: Added editor.commands

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -8,13 +8,16 @@ c.colors.tabs.even.bg = 'grey'
 c.colors.tabs.odd.bg = 'grey'
 c.tabs.show = 'switching'
 c.statusbar.show = 'in-mode'
-
-config.load_autoconfig()
+c.editor.command = ['gvim', '-f', '{file}', '-c', 'normal {line}G{column}|']
+c.editor.command = ['emacs', '+{line}:{column}', '{file}']
 
 config.bind(',t', 'config-cycle tabs.show always switching')
+config.bind(',e', 'config-cycle editor.command \'["emacs", "+{line}:{column}", "{file}"]\' \'["gvim", "-f", "{file}", "-c", "normal {line}G{column0}l"]\'')
 config.bind(',lp', 'spawn --userscript qute-pass -p')
 config.bind(',lo', 'spawn --userscript qute-pass -o')
 config.bind(',lu', 'spawn --userscript qute-pass -u')
 config.bind(',ll', 'spawn --userscript qute-pass -a')
 config.bind(',m', 'spawn mpv --audio-device=pulse --ytdl-raw-options=sub-langs="[en.*,es.*,fr.*,pt.*]",write-sub=,write-auto-sub= {url}')
 config.bind(',M', 'hint links spawn mpv --audio-device=alsa/pulse --ytdl-raw-options=sub-langs="[en.*,es.*,fr.*,pt.*]",write-sub=,write-auto-sub= {hint-url}')
+
+config.load_autoconfig()
