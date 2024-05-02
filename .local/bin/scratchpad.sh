6e48d5f7d94ff0759567b57b012338f12e474946
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2024-05-01 22:15:26 -0500

    bspwm: fixed issue in scratchpad

--- .local/bin/scratchpad.sh
+++ .local/bin/scratchpad.sh
@@ -9,7 +9,7 @@ if [ -z "$winid" ]; then
 	nohup emacs >/dev/null 2>/dev/null </dev/null &
 else
 	if [ -z "$visible_winid" ]; then
-		echo "$winid" | xargs -I{} xdo show {}
+		echo "$winid" | tac | xargs -I{} xdo show {}
 	else
 		echo "$winid" | xargs -I{} xdo hide {}
 	fi
