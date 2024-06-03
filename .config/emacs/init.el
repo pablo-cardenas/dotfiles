3190f77e59388b4ca20607799e86019a0e5de739
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2024-06-03 01:48:56 -0500

    emacs: removed menu-bar, tools-bar, toolbar-b

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,8 @@
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
