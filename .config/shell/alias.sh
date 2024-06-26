721acad8a6f7d99747488dc342fe64085ecc97d4
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2024-06-26 17:45:09 -0500

    vim: XDG Base Directory specification

--- .config/shell/alias.sh
+++ .config/shell/alias.sh
@@ -1,3 +1,3 @@
 alias ls='ls -AF'
-alias vim='vim -u ~/.vim/vimrc'
+alias vim='vim -u "${XDG_CONFIG_HOME}"/vim/vimrc'
 alias dotfiles='git --git-dir "${HOME}"/.dotfiles --work-tree "${HOME}" -C "${HOME}"'
