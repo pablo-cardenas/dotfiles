8394983ec93b2f63491a47ec0f031b2ac776ef14
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2024-07-17 21:04:33 -0500

    shell: fixed dotfiles alias (remove -C)

--- .config/shell/alias.sh
+++ .config/shell/alias.sh
@@ -1,3 +1,3 @@
 alias ls='ls -AF'
 alias vim='vim -u "${XDG_CONFIG_HOME}"/vim/vimrc'
-alias dotfiles='git --git-dir "${HOME}"/.dotfiles --work-tree "${HOME}" -C "${HOME}"'
+alias dotfiles='git --git-dir "${HOME}"/.dotfiles --work-tree "${HOME}"'
