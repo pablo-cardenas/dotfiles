c88be57630e8ed682e8d4d8faf51f04f30c16da9
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2024-08-28 19:10:28 -0500

    Removed unused configs

--- .config/polybar/scripts/taskwarrior.sh
+++
@@ -1,9 +0,0 @@
-active_id=$(task +ACTIVE rc.verbose: limit:1 id | cut -d ' ' -f2)
-next_id=$(task next rc.verbose: limit:1 id | cut -d ' ' -f2)
-
-if [ -z $active_id ]
-then
-	echo  NEXT $(task _get ${next_id}.description)
-else
-	echo  ACTIVE $(task _get ${active_id}.description)
-fi
