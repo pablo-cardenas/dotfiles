8ebd3454c0d0a2e6067f64976d03bc594d4063ea
Pablo Cárdenas <pablo.cardenas@imca.edu.pe>
2024-09-30 18:26:42 -0500

    windows: allow vim to use stdin

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,12 @@ $env:Path += ";C:\Program Files\Vim\vim91"
 function prompt {"$ "}
 
 function vim { 
-	$external_vim=Get-Command -Type Application vim
+	$external_vim = Get-Command -Type Application vim
+	if ($MyInvocation.ExpectingInput) {
+		$input | & $external_vim.Source -u $env:XDG_CONFIG_HOME/vim/vimrc --cmd "set packpath+=$env:XDG_CONFIG_HOME\vim" $args
+	} else {
 	& $external_vim.Source -u $env:XDG_CONFIG_HOME/vim/vimrc --cmd "set packpath+=$env:XDG_CONFIG_HOME\vim" $args 
+	}
 }
+
 function dotfiles { git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" $args }
